f6e6e739d36f7ebbf653cb7e88cc9fb3e6501ed3
Pohang <omjipanggg@yahoo.com>
2023-06-20 12:48:44 +0700

    Onero ingravesco

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -17,7 +17,7 @@
             {{ __('or ') }}
             <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                 @csrf
-                <button type="submit" class="btn btn-link p-0 m-0 align-baseline dotted">{{ __('click here') }}</button>
+                <button type="submit" class="btn btn-link p-0 m-0 align-baseline dotted" onclick="showLoading()">{{ __('click here') }}</button>
             </form>
             {{ __(' to request another.') }}
             </div>
