028c95db833fdbe51af5e27bb8d184be1351b72c
Pohang <omjipanggg@yahoo.com>
2023-06-24 23:55:07 +0700

    Magis magnus maximus

--- database/migrations/2023_06_18_060843_create_role_users_table.php
+++ database/migrations/2023_06_18_060843_create_role_users_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Model\User;
+
 return new class extends Migration
 {
     /**
@@ -15,11 +17,13 @@ return new class extends Migration
             $table->id();
             $table->foreignUuid('user_id')->cascadeOnUpdate()->constrained();
             $table->foreignId('role_id')->cascadeOnUpdate()->constrained();
-            $table->dateTime('expire_date')->default('2049-12-31 23:59:59');
+            $table->dateTime('expire_date')->default('2023-12-31 23:59:59');
             $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
             $table->softDeletes();
         });
+
+        User::findOrFail('3e77f9a9-fa67-43c4-84a0-6253090b17fc')->roles()->attach(1);
     }
 
     /**
