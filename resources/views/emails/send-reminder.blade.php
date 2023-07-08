e5f0e80a03627b51abc6816abc72587f4923d826
Pohang <omjipanggg@yahoo.com>
2023-07-08 12:16:41 +0700

    Onero ingravesco

--- resources/views/emails/send-reminder.blade.php
+++ resources/views/emails/send-reminder.blade.php
@@ -1,14 +1,17 @@
 @component('mail::message')
 ### Henlo, Webmaster!
-### This is a brief resume of a newly registered user at {{ config('app.name') }}:
+
+This is a brief resume of a newly registered user at {{ config('app.name') }}:
 
 @component('mail::table')
-| **Name** | {{ \Str::headline($data['name']) }} |
-| **Email** | {{ \Str::lower($data['email']) }} |
-| **Verified** | {{ date('F d, Y', strtotime($data['email_verified_at'])) ?? '**false**' }} |
+| **Name**       | {{ \Str::headline($data['name']) }} |
+| :------------- | :---------------------------------- |
+| **Email**      | {{ \Str::lower($data['email']) }} |
+| **Verified**   | {{ date('F d, Y', strtotime($data['email_verified_at'])) ?? '**false**' }} |
 | **Registered** | {{ date('F d, Y', strtotime($data['created_at'])) }} |
 @endcomponent
 
 Mucho gracias,
+<br>
 {{ config('app.name') }}
 @endcomponent
