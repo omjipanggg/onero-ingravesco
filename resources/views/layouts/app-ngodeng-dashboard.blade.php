8e1919c6ac93f5f598483bda0408cb5426f7ac68
Pohang <omjipanggg@yahoo.com>
2023-09-02 15:26:45 +0700

    Layouts updated

--- resources/views/layouts/app-ngodeng-dashboard.blade.php
+++ resources/views/layouts/app-ngodeng-dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Pohang">
-    <meta name="description" content="AAAAAAAAA">
+    <meta name="description" content="AAAAAAAAAAAAAA">
     <meta name="keywords" content="laravel, web, application">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
